dac17cda6fc8737b129ded38ac80353995ce9e56
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-16 13:17:31 +0700

    feat: create new partnership adaptor service

---
+++ recommendations/internal/recommendation/adapter.go
@@ -0,0 +1,25 @@
+package recommendation
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+type PartnershipAdaptor struct {
+	client *http.Client
+	url    string
+}
+
+func NewPartnershipAdaptor(client *http.Client, url string) (*PartnershipAdaptor, error) {
+	if client == nil {
+		return nil, errors.New("client can't be nil")
+	}
+
+	if url == "" {
+		return nil, errors.New("url can't be empty")
+	}
+
+	return &PartnershipAdaptor{client: client, url: url}, nil
+}
